c8027111da1704323646d5d4923b8cf4cb0775d3
Mobina Toorani <mobinatoorani@gmail.com>
2023-01-24 16:20:43 -0500

    Created Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import {Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ImageUpload from './components/ImageUpload';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
+      <Route path="/" element={<LoginPage/>} />
       <Route path = "/grading" element = {<ImageUpload/>} />
     </Routes>
+    </BrowserRouter>
   );
 }
 
